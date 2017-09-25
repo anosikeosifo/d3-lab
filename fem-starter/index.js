85a8ed60f4c60aa440367dd4d742973e4c53ef61
Anosike Osifo <code.anosikeosifo@gmail.com>
2017-09-25 05:53:03 +0100

    fem-starter -- Complete chart

--- fem-starter/index.js
+++ fem-starter/index.js
@@ -12,26 +12,31 @@ d3.tsv("data.tsv", (err, data) => {
     d.date = d3.timeParse("%Y%m%d")(d.date);
     d.date = new Date(d.date);
     d[city] = ++d[city];
-
-    const svg = d3.select("svg");
-    const yMax = d3.max(data, d => d[city]);
-    const xScale = d3.scaleTime().domain(d3.extent(data, d => d.date)).range([margin.left, width - margin.right]);
-    const yScale = d3.scaleLinear().domain([0, yMax]).range([height - margin.bottom, margin.top]);
-
-    const xAxis = d3.axisBottom().scale(xScale).tickFormat(d3.timeFormat("%b %Y"));
-    const yAxis = d3.axisLeft().scale(yScale);
-
-    svg.append("g").attr("transform", `translate(${[0, height - margin.bottom]})`).call(xAxis);
-    svg.append("g").attr("transform", `translate(${[margin.left, 0]})`).call(yAxis);
-
-    const rect = svg
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("width", 5)
-      .attr("x", d => xScale(d.date))
-      .attr("y", d => yScale(d[city]))
-      .attr("height", d => height - yScale(d[city]));
   });
+
+  const svg = d3.select("svg");
+  const yMax = d3.max(data, d => d[city]);
+  const yExtent = d3.extent(data, d => d[city]);
+
+  const xScale = d3.scaleTime().domain(d3.extent(data, d => d.date)).range([margin.left, width - margin.right]);
+  const yScale = d3.scaleLinear().domain(d3.extent(data, d => d[city])).range([height, margin.top]);
+
+  const xAxis = d3.axisBottom().scale(xScale).tickFormat(d3.timeFormat("%b %Y"));
+  const yAxis = d3.axisLeft().scale(yScale);
+
+  svg.append("g").attr("transform", 'translate(' + [0, height] + ')').call(xAxis);
+  svg.append("g").attr("transform", 'translate(' + [margin.left, 0] + ')').call(yAxis);
+
+  const rect = svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("width", 2)
+    .attr('fill', '#ffa600')
+    .attr('stroke', 'teal')
+    .attr('strokeWidth', 0.5)
+    .attr("x", d => xScale(d.date))
+    .attr("y", d => yScale(d[city]))
+    .attr("height", d => height - yScale(d[city]));
 });
